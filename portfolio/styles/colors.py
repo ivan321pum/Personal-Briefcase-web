996c184ba6ef453ca8887172f830f93fc5e8c4ce
Iván Sevilla Gómez <ivansevillagomez@protonmail.com>
2024-04-01 23:01:37 +0200

    bloques about me ordenaditos

--- portfolio/styles/colors.py
+++ portfolio/styles/colors.py
@@ -6,3 +6,7 @@ class Color(Enum):
     color_black = "#000000"
     color_blue = "#73cefa"
     color_red = "#CD5C5C"
+    color_grey = "#C0C0C0"
+    color_salmon = "#FFA07A"
+    color_green = "#66CCCC"
+    color_yellow = "#FFD700"
--- portfolio/views/about_me.py
+++ portfolio/views/about_me.py
@@ -6,27 +6,97 @@ from portfolio.styles.colors import Color
 
 def about_me() -> rx.Component:
     return rx.box(
-        rx.text("ABOUT ME", font_family=Font.TITLE.value, font_size=Size.BIG.value, align="center"),
+        rx.text("ABOUT ME", font_family=Font.TITLE.value, font_size=Size.VERY_BIG.value, align="center"),
         rx.flex(
-            rx.card(
-                rx.dialog.root(
-                    rx.dialog.trigger(
-                        rx.link(
-                            rx.box(
-                                "CODING",
-                                background="center/cover url('https://i.blogs.es/fd396a/hook/450_1000.jpg')",
+            rx.grid(
+                rx.card(
+                    rx.dialog.root(
+                        rx.dialog.trigger(
+                            rx.link(
+                                rx.box(
+                                    rx.heading("PROGRAMMING PASSION", as_="h1", align="center", style={"color": Color.color_black.value}),
+                                ),
                             ),
-                        )
-                    ),
-                    rx.dialog.content(
-                        rx.vstack(
-                            rx.text("MI PASIÓN POR EL CÓDIGO", as_="p"),
-                            rx.dialog.close(
-                                rx.button("Cierra")
+                        ),
+                        rx.dialog.content(
+                            rx.vstack(
+                                rx.text("Student Life"),
+                                rx.dialog.close(
+                                    rx.button("Cierra")
+                                ),
                             ),
                         ),
                     ),
+                    background=Color.color_yellow.value,
                 ),
+                rx.card(
+                    rx.dialog.root(
+                        rx.dialog.trigger(
+                            rx.link(
+                                rx.box(
+                                    rx.heading("STUDENT LIFE", as_="h1", align="center", style={"color": Color.color_black.value}),
+                                ),
+                            )
+                        ),
+                        rx.dialog.content(
+                            rx.vstack(
+                                rx.text("MI PASIÓN POR EL CÓDIGO"),
+                                rx.dialog.close(
+                                    rx.button("Cierra")
+                                ),
+                            ),
+                        ),
+                    ),
+                    background=Color.color_green.value,
+                ),
+                rx.card(
+                    rx.dialog.root(
+                        rx.dialog.trigger(
+                            rx.link(
+                                rx.box(
+                                    rx.heading("PERSONAL GOALS", as_="h1", align="center", style={"color": Color.color_black.value}),
+                                ),
+                            )
+                        ),
+                        rx.dialog.content(
+                            rx.vstack(
+                                rx.text("MI PASIÓN POR EL CÓDIGO"),
+                                rx.dialog.close(
+                                    rx.button("Cierra")
+                                ),
+                            ),
+                        ),
+                    ),
+                    background=Color.color_salmon.value,
+                ),
+                rx.card(
+                    rx.dialog.root(
+                        rx.dialog.trigger(
+                            rx.link(
+                                rx.box(
+                                    rx.heading("PERSONALITY & INTERESTS", as_="h1", align="center", style={"color": Color.color_black.value}),
+                                ),
+                            )
+                        ),
+                        rx.dialog.content(
+                            rx.vstack(
+                                rx.text("MI PASIÓN POR EL CÓDIGO"),
+                                rx.dialog.close(
+                                    rx.button("Cierra")
+                                ),
+                            ),
+                        ),
+                    ),
+                    background=Color.color_grey.value,
+                ),
+                columns="2",
+                spacing="8",
+                justify="center",
+                width="50%",
             ),
+            justify="center",
+            align="center",
+            width="100%",
+            height="100%",
         ),
     )
